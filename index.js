8565ef34b156ad8f013761eeffa64f4c64fdfb9b
Anaelle Cauvin <anaelle.cauvin@epitech.eu>
2023-10-03 10:20:24 +0200

    [ADD] rainbow names

---
+++ index.js
@@ -0,0 +1,7 @@
+import chalk from "chalk";
+
+console.log(chalk.blue("Raphaël"));
+console.log(chalk.red("Florian"));
+console.log(chalk.green("Antoine"));
+console.log(chalk.yellow("Mahdi"));
+console.log(chalk.magenta("Cassiopée"));
